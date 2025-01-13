dca466ea20a8cc361195645367f486969f3b2319
UNDERSTOOD? <sampervlad373@gmail.com>
2025-01-13 12:01:14 +0200

    Update Автообнова.py

--- Автообнова.py
+++ Автообнова.py
@@ -3,7 +3,7 @@ import requests
 
 file_name = "Удалятор.Deleter.py"
 download_url = "https://raw.githubusercontent.com/merfiDEV/Z/main/Удалятор.Deleter.py"
-save_path = f"/storage/emulated/0/merfiDEV/{file_name}"  # Изменено на /merfiDEV/
+save_path = f"/storage/emulated/0/merfiDEV/{file_name}" 
 
 languages = {
     1: {
